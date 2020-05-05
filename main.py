1ed85a905d69bbe6e168d8399c41f3b475889073
Takashi Yamashina <takashi.yamashina@gmail.com>
2020-05-05 06:41:23 +0000

    add CORS

--- main.py
+++ main.py
@@ -11,10 +11,24 @@ conn = mysql.connector.connect(
     database=os.getenv('MYSQL_DB', 'sample')
 )
 
-app = Flask(__name__)
+def select_by_query(request):
+    # For more information about CORS and CORS preflight requests, see
+    # https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+    # for more information.
 
-@app.route('/')
-def index():
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600',
+        }
+
+        return ('', 204, headers)
+
     q = request.args.get('q')
 
     conn.ping(reconnect=True)
@@ -33,10 +47,32 @@ def index():
             town_name=item[6],
             town_name_kana=item[7]
         ))
-    return jsonify(results)
+    
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+    }
+    
+    return (jsonify(results), 200, headers)
 
-@app.route('/{zipcode}')
-def find(zipcode):
+def find_by_zipcode(request, zipcode):
+    # For more information about CORS and CORS preflight requests, see
+    # https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+    # for more information.
+    
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600',
+        }
+
+        return ('', 204, headers)
+
     conn.ping(reconnect=True)
     cur = conn.cursor()
     cur.execute("SELECT * FROM address WHERE zipcode = '{}'".format(zipcode))
@@ -53,7 +89,24 @@ def find(zipcode):
             town_name=item[6],
             town_name_kana=item[7]
         ))
-    return jsonify(results)
+    
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+    }
+
+    return (jsonify(results), 200, headers)
 
 if __name__ == '__main__':
+
+    app = Flask(__name__)
+
+    @app.route('/', methods=['GET','OPTIONS'])
+    def index():
+        return select_by_query(request)
+
+    @app.route('/{zipcode}', methods=['GET','OPTIONS'])
+    def find(zipcode):
+        return find_by_zipcode(request, zipcode)
+
     app.run()
